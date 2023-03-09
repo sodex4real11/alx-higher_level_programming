168330e429747d48c3fe054f7f9ecd4318d7da02
sodex4real11 <sodex4real11@gmail.com>
2023-03-08 16:41:53 -0800

    Python project

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for char in range(122, 96, -1):
-    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
