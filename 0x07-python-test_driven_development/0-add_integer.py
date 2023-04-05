3a28d957c995945c87bfb4b427240eb85d76e7c2
sodex4real11 <sodex4real11@gmail.com>
2023-04-05 04:23:52 -0700

    Test Driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-This script defines a function for integer addition.
-"""
-
+"""This script defines a function for integer addition."""
 
 def add_integer(a, b=98):
-    """
-    This function returns the integer addition of a and b.
-    If either a or b is a non-integer and non-float, TypeError is raised.
-    Float arguments are typecasted.
+    """ Function that adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
     """
 
-    # Check if a is not an integer or a float
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-
-    # Check if b is not an integer or a float
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-
-    # Cast a and b to integers and return their sum
-    return int(a) + int(b)
+    a = int(a)
+    b = int(b)
+    return (a + b)
