05728836e3ef45763c81301db5698fe3ee193c55
sodex4real11 <sodex4real11@gmail.com>
2023-04-11 23:17:13 -0700

    Python inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-"""
-more class base
+"""Defines a class Square based on 9-rectangle.py.
+Attributes:
+    width (int): width of the rectangle.
+    height (int): height of the rectangle.
 """
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
+    """Defines a class Square.
+    Args:
+        Rectangle (Rectangle): rectangle
+    """
+
     def __init__(self, size):
-        """ size init"""
+        """Creates new instances of class Square.
+        Args:
+            size (int): size of 1 side of square.
+        """
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+        super().__init__(size, size)
 
     def __str__(self):
-        return("[Square] " + str(self.__size) + "/" + str(self.__size))
+        """Returns a string representation of the square.
+        Returns:
+            str: square.
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """Calculates the area of a square.
+        Returns:
+            int: the area of the square.
+        """
+        return self.__size ** 2
