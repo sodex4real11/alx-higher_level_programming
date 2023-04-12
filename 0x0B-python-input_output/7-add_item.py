0442b6336266bbd69f3a29afe61a9b23e08ac2af
sodex4real11 <sodex4real11@gmail.com>
2023-04-12 11:15:51 -0700

    Input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 import json
 from sys import argv
-"""
-This file contains a function that adds
-all arguments to a python list and saves
-to a file
-"""
+"""This file contains a function that adds all arguments to a python list and saves
+to a file"""
 
 
-save_json = __import__('5-save_to_json_file').save_to_json_file
-load_json = __import__('6-load_from_json_file').load_from_json_file
+    save_json = __import__('5-save_to_json_file').save_to_json_file
+    load_json = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'add_item.json'
+    filename = 'add_item.json'
 
-try:
-    new = load_json(filename)
-except (ValueError, FileNotFoundError):
-    new = []
+    try:
+        new = load_json(filename)
+    except (ValueError, FileNotFoundError):
+        new = []
 
-new = new + argv[1:]
-save_json(new, filename)
+    new = new + argv[1:]
+    save_json(new, filename)
