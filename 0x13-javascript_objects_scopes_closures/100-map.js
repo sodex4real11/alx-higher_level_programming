3d8a3736c499b1b4e53b6a95d148c49ae9a567a6
sodex4real11 <sodex4real11@gmail.com>
2023-06-14 08:30:02 -0700

    Javascript 2

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) 
-{
-  return num * index;
-));
+const list = require('./100-data.js').list;
 
+const newList = list.map((val, idx) => val * idx);
 console.log(list);
 console.log(newList);
