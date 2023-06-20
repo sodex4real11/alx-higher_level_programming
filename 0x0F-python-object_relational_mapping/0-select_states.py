8e71670dd99277d668a83d65e1520fec021491b5
sodex4real11 <sodex4real11@gmail.com>
2023-06-20 04:12:58 -0700

    ORM tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
-import sys
-
+from sys import argv
 
 if __name__ == '__main__':
-
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-        )
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
-    table = cur.fetchall()
+    rows = cur.fetchall()
 
-    for row in table:
+    for row in rows:
         print(row)
-
-    cur.close()
-    db.close()
